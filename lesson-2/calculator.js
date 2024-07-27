8a7b93e2a7330107251dee26bb0d574034624d6e
William Martinez <wrmartinez709@gmail.com>
2024-07-27 14:33:18 -0400

    create lesson-2 directory and initial setup for CLI calculator

---
+++ lesson-2/calculator.js
@@ -0,0 +1,5 @@
+// Ask the user for the first number
+// Ask the user for the second number
+// Ask the user for an operation to perform
+// Perfom the operation on the two numbers
+// Print the result to the terminal
