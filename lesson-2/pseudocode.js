3f1e0472287c7e3b207af4d1dce7ed49c4579220
William Martinez <wrmartinez709@gmail.com>
2024-07-27 21:50:28 -0400

    create pseudocode.js and write pseudocode for first two problems

---
+++ lesson-2/pseudocode.js
@@ -0,0 +1,27 @@
+/* 
+
+# function that returns the sum of two numbers
+
+START
+- declare a function with two parameters (numbers)
+**params number1, number
+- SET sum = number1 + number2
+- PRINT sum
+- call the function
+- END
+
+*/
+
+/*
+
+# Given an array of strings, return a string with all the strings concatenated together
+
+START
+- declare a function with a single parameter (array)
+**params strArr
+SET joinedString = strArr.join(".");
+PRINT joinedString
+- call the function
+END
+
+*/
